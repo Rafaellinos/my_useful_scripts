b832683da8677e59f756711eb9e1785ee2dfd9e2
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-03-06 12:20:03 -0300

    Add if argument to use a specific db

--- start_odoo.sh
+++ start_odoo.sh
@@ -1,5 +1,13 @@
 # !/urs/bin/env bash
 
-. /home/rafael/odoo/odoo13/odoo13_env/bin/activate --python=python3 venv
 
-/home/rafael/odoo/odoo13/odoo/odoo-bin --addons-path='~/odoo/odoo13/odoo/addons, ~/odoo/odoo13/odoo/custom-addons, ~/Documents/lliege_work/lliege_documentos' -r'odoo13' -w'odoo'
+
+. /home/$(whoami)/odoo/odoo13/odoo13_env/bin/activate --python=python3 venv
+
+
+if [[ $1 != "" ]]; then
+	/home/$(whoami)/odoo/odoo13/odoo/odoo-bin --addons-path='/home/lliege/odoo/odoo13/odoo/addons,/home/lliege/odoo/odoo13/odoo/odoo/addons, /home/lliege/Projects/odoo_13_modules, /home/lliege/Projects/lliege_login_redirect,/home/lliege/Projects' -r'odoo13' -w'odoo' -d $1
+else
+	/home/$(whoami)/odoo/odoo13/odoo/odoo-bin --addons-path='/home/lliege/odoo/odoo13/odoo/addons,/home/lliege/odoo/odoo13/odoo/odoo/addons, /home/lliege/Projects/odoo_13_modules, /home/lliege/Projects/lliege_login_redirect,/home/lliege/Projects' -r'odoo13' -w'odoo'
+fi
+
